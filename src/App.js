a90ca58e35c16ec812e124af0262f545355308b5
RammiyaBaskar <rammiya.sb@gmail.com>
2025-03-31 23:13:28 +0530

    images and card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
       <Header />
      <HomeScreen/>
       <Footer />
+      </BrowserRouter>
     </div>
   );
 };
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,13 +4,94 @@ import FoodCard from "../components/FoodCard";
 import axios from 'axios';
 import { RESTAURANTS } from '../constants'; 
 
+import { Col, Container, Row } from 'react-bootstrap'
 
-type Food = {
-  name: string;
-  category: string;
-  price: number;
-  image: string;
-}
+
+import Image1 from "../assets/menu/burger-11.jpg"
+import Image2 from "../assets/menu/burger-12.jpg"
+import Image3 from "../assets/menu/burger-13.jpg"
+import Image4 from "../assets/menu/burger-14.jpg"
+import Image5 from "../assets/menu/burger-15.jpg"
+import Image6 from "../assets/menu/burger-16.jpg"
+import Image7 from "../assets/menu/burger-17.jpg"
+import Image8 from "../assets/menu/burger-18.jpg"
+import Card from '../components/Card'
+
+
+// type Food = {
+//   name: string;
+//   category: string;
+//   price: number;
+//   image: string;
+// }
+
+const mockData = [
+  {
+    id: "0001",
+    image: Image1,
+    title: "Crispy Chicken",
+    paragraph: "Chicken breast, chilli sauce, tomatoes, pickles, coleslaw",
+    rating: 5,
+    price: 99.15,
+  },
+  {
+    id: "0002",
+    image: Image2,
+    title: "Ultimate Bacon",
+    paragraph: "House patty, cheddar cheese, bacon, onion, mustard",
+    rating: 4.5,
+    price: 99.32,
+  },
+  {
+    id: "0003",
+    image: Image3,
+    title: "Black Sheep",
+    paragraph: "American cheese, tomato relish, avocado, lettuce, red onion",
+    rating: 4,
+    price: 69.15,
+  },
+  {
+    id: "0004",
+    image: Image4,
+    title: "Vegan Burger",
+    paragraph: "House patty, cheddar cheese, bacon, onion, mustard",
+    rating: 3.5,
+    price: 99.25,
+  },
+  {
+    id: "0005",
+    image: Image5,
+    title: "Double Burger",
+    paragraph: "2 patties, cheddar cheese, mustard, pickles, tomatoes",
+    rating: 3.0,
+    price: 59.25,
+  },
+  {
+    id: "0006",
+    image: Image6,
+    title: "Turkey Burger",
+    paragraph: "Turkey, cheddar cheese, onion, lettuce, tomatoes, pickles",
+    rating: 3,
+    price: 79.18,
+  },
+  {
+    id: "0007",
+    image: Image7,
+    title: "Smokey House",
+    paragraph: "patty, cheddar cheese, onion, lettuce, tomatoes, pickles",
+    rating: 2.5,
+    price: 99.19,
+  },
+  {
+    id: "0008",
+    image: Image8,
+    title: "Classic Burger",
+    paragraph: "cheddar cheese, ketchup, mustard, pickles, onion",
+    rating: 2.0,
+    price: 89.12,
+  },
+  // Add more mock data objects as needed
+];
 // const foodItem = {
 //   name: "Cheese Burger",
 //   description: "A delicious cheeseburger with fresh ingredients.",
@@ -20,35 +101,87 @@ type Food = {
 const HomeScreen = () => {
   const [Menu,setMenu]=useState([]);
   const [restaurant,setRestaurant] = useState(""); 
-    useEffect(() => {
-                axios.get(RESTAURANTS)
-            .then((response)=>
-            {
-                console.log(response.data);
-                setRestaurant(response.data);
+  const renderRatingIcons = (rating) => {
+      const stars = [];
+      for(let i = 0; i<5; i++)
+      {
+          if(rating>0.5)
+          {
+              stars.push(<i key={i} className="bi bi-star-fill"></i>);
+              rating--;
+          }
+          else if(rating>0 && rating<1)
+          {
+              stars.push(<i key={"half"} className="bi bi-star-half"></i>);
+              rating--;
 
-               console.log("data===",restaurant);
-             //  console.log("data.restaurants===",restaurants.restaurants);
-             restaurant?.restaurants?.map((restaurant) => 
-               (console.log("restaurant.name===",restaurant.name)));
+          }
+          else{
+              stars.push(<i key={`empty${i}`} className="bi bi-star"></i>);
+          }
+      }
+      return stars;
+  }
+
+
+    // useEffect(() => {
+    //             axios.get(RESTAURANTS)
+    //         .then((response)=>
+    //         {
+    //             console.log(response.data);
+    //             setRestaurant(response.data);
+
+    //            console.log("data===",restaurant);
+    //          //  console.log("data.restaurants===",restaurants.restaurants);
+    //          restaurant?.restaurants?.map((restaurant) => 
+    //            (console.log("restaurant.name===",restaurant.name)));
                
 
-                //   const firstRestaurantMenu = response.data.restaurants[0].menu;
-                // console.log("First restaurant menu:", firstRestaurantMenu);
-                //  setMenu(firstRestaurantMenu);
-                //  console.log("Menu:=====", Menu);
+    //             //   const firstRestaurantMenu = response.data.restaurants[0].menu;
+    //             // console.log("First restaurant menu:", firstRestaurantMenu);
+    //             //  setMenu(firstRestaurantMenu);
+    //             //  console.log("Menu:=====", Menu);
               
-            }).catch((error)=>
-            {
-                console.log(error);
-            });
+    //         }).catch((error)=>
+    //         {
+    //             console.log(error);
+    //         });
             
-    });	
+    // });	
   return (
-    restaurant.restaurants?.map((restaurant) => (
-      <div key={restaurant.id}>
-        <h2>{restaurant.name}</h2>
-        <ul>
+   // restaurant.restaurants?.map((restaurant) => (
+      // <div key={restaurant.id}>
+      //   <h2>{restaurant.name}</h2>
+      <div className="home_screen">
+        <Container>
+            <Row>
+                <Col lg={{span:8,offset:2}} className='text-center mb-5'>
+                    <h2>Our Burgers</h2>
+                    <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore doloremque sed tempore officiis laboriosam omnis sapiente hic delectus consequuntur illum!</p>
+                    
+                </Col>
+            </Row>
+
+            <Row>
+                {
+                    mockData.map((cardData,index)=>(
+                        <Card key={index} 
+                        image={cardData.image} 
+                        title={cardData.title}
+                        rating = {cardData.rating}
+                        para = {cardData.paragraph}
+                        price = {cardData.price}
+                        renderRatingIcons = {renderRatingIcons}
+                        />
+                    )
+                )}
+                
+            </Row>
+
+           
+
+        </Container>
+        {/* <ul>
           {restaurant.menu.map((item) => (
             <li key={item.id}>
               <strong>{item.name}</strong> - ${item.price} ({item.category})
@@ -61,9 +194,9 @@ const HomeScreen = () => {
               />
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
-    ))
+   // ))
   //   <div>
       
   //  {/* // <FoodCard food={Food} /> */}
